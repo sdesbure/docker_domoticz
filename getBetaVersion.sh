9e9da3f6f15114b40c97a1937196768f93d10e3c
Cedric Gatay <c.gatay@code-troopers.com>
2017-01-19 21:16:17 +0100

    feat(getBetaRelease): getBetaRelease script
    Added beta release script

---
+++ getBetaVersion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl --silent https://releases.domoticz.com/releases/beta/version_linux_x86_64.h | awk '{print $2"="$3}' > beta.env
+source beta.env
+git checkout -b 3.$APPVERSION-beta
+sed -i "s/ARG BRANCH_NAME/ENV BUILD_BRANCH=$APPHASH/g" Dockerfile
