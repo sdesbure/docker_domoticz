b216410c40bfe80b040af4a6a7a356dd05324113
Cedric Gatay <c.gatay@code-troopers.com>
2017-01-24 08:28:09 +0100

    chore(): getBetaVersion is now committing
    Less error prone with an autocommitting script

--- getBetaVersion.sh
+++ getBetaVersion.sh
@@ -3,3 +3,5 @@ curl --silent https://releases.domoticz.com/releases/beta/version_linux_x86_64.h
 source beta.env
 git checkout -b 3.$APPVERSION-beta
 sed -i "s/ARG BRANCH_NAME/ENV BUILD_BRANCH=$APPHASH/g" Dockerfile
+git add Dockerfile
+git commit -m"beta(): bump to 3.$APPVERSION-beta"
