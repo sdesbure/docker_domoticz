b815e368c5e0625d2cc33c190c8d64ffb70bfacf
Cedric Gatay <c.gatay@code-troopers.com>
2017-01-24 17:32:46 +0100

    chore(): better script

--- getBetaVersion.sh
+++ getBetaVersion.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl --silent https://releases.domoticz.com/releases/beta/version_linux_x86_64.h | awk '{print $2"="$3}' > beta.env
 source beta.env
+rm beta.env
 git branch -r | grep "3.$APPVERSION-beta"
 EXIST=$?
 if [[ $EXIST != "0" ]]; then
@@ -8,5 +9,5 @@ git checkout -b 3.$APPVERSION-beta
 sed -i "s/ARG BRANCH_NAME/ENV BUILD_BRANCH=$APPHASH/g" Dockerfile
 git add Dockerfile
 git commit -m"beta(): bump to 3.$APPVERSION-beta"
-git push -u origin
+git push -u origin 3.$APPVERSION-beta
 fi;
